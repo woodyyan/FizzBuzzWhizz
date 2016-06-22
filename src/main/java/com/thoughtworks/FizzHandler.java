c529382893965479385c76fccd917c6fce0d2216
WoodyYan <sbyan@CNsbyan.local>
2016-06-22 16:34:57 +0800

    Task 2 done.

--- src/main/java/com/thoughtworks/FizzHandler.java
+++ src/main/java/com/thoughtworks/FizzHandler.java
@@ -1,8 +1,17 @@
 package com.thoughtworks;
 
 public class FizzHandler {
-    public static String getResult(int number) {
+    public String getFizzIfTimesOfThree(int number) {
         if (number % 3 == 0) {
+            return "Fizz";
+        }
+        return null;
+    }
+
+    public String getFizzIfContainsThree(int number) {
+        if (number % 10 == 3) {
+            return "Fizz";
+        } else if (number % 100 / 10 == 3) {
             return "Fizz";
         }
         return null;
